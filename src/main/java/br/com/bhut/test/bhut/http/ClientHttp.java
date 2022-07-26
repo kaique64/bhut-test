3c8f3b5be648c93f72a2b9fe96557c42ba9d857f
kaiqueh.2231 <kaiqueh.2231@gmail.com>
2022-07-26 19:23:03 -0300

    feat: create methods to make HTTP request and get your response

--- src/main/java/br/com/bhut/test/bhut/http/ClientHttp.java
+++ src/main/java/br/com/bhut/test/bhut/http/ClientHttp.java
@@ -1,4 +1,39 @@
 package br.com.bhut.test.bhut.http;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ClientHttp {
+
+    /**
+     * Function responsible to make the HTTP request
+     * @param url the url from the service
+     */
+    private HttpResponse<String> makeRequest(String url) {
+        try {
+            URI address = URI.create(url);
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder(address).GET().build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            return response;
+        } catch (IOException | InterruptedException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /**
+     * Function responsible to get the response from HTTP request
+     * @param url the url from the service
+     */
+    public String searchData(String url) {
+        HttpResponse<String> response = makeRequest(url);
+
+        String body = response.body();
+        return body;
+    }
+
 }
