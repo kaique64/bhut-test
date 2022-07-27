aeb6f5295438dd3ce777461504c08497d70470b7
kaiqueh.2231 <kaiqueh.2231@gmail.com>
2022-07-27 15:28:47 -0300

    feat: create value object to car entity

---
+++ src/main/java/br/com/bhut/test/bhut/vo/CarVO.java
@@ -0,0 +1,20 @@
+package br.com.bhut.test.bhut.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class CarVO {
+
+    private String _id;
+
+    private String title;
+
+    private String brand;
+
+    private String price;
+
+    private Integer age;
+
+}
