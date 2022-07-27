3333747169ce416704890a8ac7b99de738eebc15
kaiqueh.2231 <kaiqueh.2231@gmail.com>
2022-07-26 22:27:00 -0300

    refactor: remove the unnecessary imports

--- src/main/java/br/com/bhut/test/bhut/service/ListCarsService.java
+++ src/main/java/br/com/bhut/test/bhut/service/ListCarsService.java
@@ -1,16 +1,9 @@
 package br.com.bhut.test.bhut.service;
 
-import br.com.bhut.test.bhut.entity.Car;
 import br.com.bhut.test.bhut.http.ClientHttp;
-import br.com.bhut.test.bhut.repository.CarRepository;
 import br.com.bhut.test.bhut.utils.JsonParser;
 import org.json.JSONArray;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class ListCarsService {
