32282882c574d5fdec74a6f5123b918cd5de8c26
kaiqueh.2231 <kaiqueh.2231@gmail.com>
2022-07-27 15:09:55 -0300

    feat: create method to make post request with a json body

--- src/main/java/br/com/bhut/test/bhut/http/ClientHttp.java
+++ src/main/java/br/com/bhut/test/bhut/http/ClientHttp.java
@@ -1,5 +1,7 @@
 package br.com.bhut.test.bhut.http;
 
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -9,7 +11,7 @@ import java.net.http.HttpResponse;
 public class ClientHttp {
 
     /**
-     * Function responsible to make the HTTP request
+     * Function responsible to make the HTTP request (GET)
      * @param url the url from the service
      */
     private HttpResponse<String> get(String url) {
@@ -26,14 +28,31 @@ public class ClientHttp {
     }
 
     /**
+     * Function responsible to make the HTTP request (POST)
+     * @param url the url from the service
+     * @param body the POST body in JSON format
+     */
+    public HttpResponse<String> post(String url, JSONObject body) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(body.toString()))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return response;
+    }
+
+    /**
      * Function responsible to get the response from HTTP request
      * @param url the url from the service
      */
     public String searchData(String url) {
         HttpResponse<String> response = get(url);
 
-        String body = response.body();
-        return body;
+        return response.body();
     }
 
 }
