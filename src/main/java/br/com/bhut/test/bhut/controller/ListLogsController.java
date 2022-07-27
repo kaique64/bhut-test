622096d4ba86576447577dc6863e5863a229be09
kaiqueh.2231 <kaiqueh.2231@gmail.com>
2022-07-26 23:21:21 -0300

    feat: create controller to list the logs

---
+++ src/main/java/br/com/bhut/test/bhut/controller/ListLogsController.java
@@ -0,0 +1,26 @@
+package br.com.bhut.test.bhut.controller;
+
+import br.com.bhut.test.bhut.entity.Log;
+import br.com.bhut.test.bhut.service.ListLogsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/logs")
+public class ListLogsController {
+
+    @Autowired
+    private ListLogsService service;
+
+    @GetMapping
+    public List<Log> list() {
+        List<Log> logs = service.list();
+
+        return logs;
+    }
+
+}
