255ce077df7ecdac24cc70f1da2e0e9a00f1456b
kaiqueh.2231 <kaiqueh.2231@gmail.com>
2022-07-26 23:20:32 -0300

    refactor: change the collection name to 'logs'

--- src/main/java/br/com/bhut/test/bhut/entity/Log.java
+++ src/main/java/br/com/bhut/test/bhut/entity/Log.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 
@@ -12,6 +13,7 @@ import java.time.LocalDate;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Document(collection = "logs")
 public class Log {
 
     @Id
