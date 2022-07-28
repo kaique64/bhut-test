32a3da6cf196bcdaaa818814ad7efd95a8812b6e
kaiqueh.2231 <kaiqueh.2231@gmail.com>
2022-07-28 15:33:13 -0300

    feat: add rabbitmq service to car controller and send a message

--- src/main/java/br/com/bhut/test/bhut/controller/CarController.java
+++ src/main/java/br/com/bhut/test/bhut/controller/CarController.java
@@ -1,8 +1,10 @@
 package br.com.bhut.test.bhut.controller;
 
+import br.com.bhut.test.bhut.constants.RabbitMQConstants;
 import br.com.bhut.test.bhut.dto.CarDto;
 import br.com.bhut.test.bhut.service.CreateCarService;
 import br.com.bhut.test.bhut.service.ListCarsService;
+import br.com.bhut.test.bhut.service.RabbitMQService;
 import br.com.bhut.test.bhut.vo.CarVO;
 import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +26,9 @@ public class CarController {
     @Autowired
     private CreateCarService createCarService;
 
+    @Autowired
+    private RabbitMQService rabbitMQService;
+
     @GetMapping("/listCars")
     public ResponseEntity<Object> list() {
         JSONArray cars = listCarsService.list();
@@ -34,6 +39,9 @@ public class CarController {
     @PostMapping("/createCar")
     public ResponseEntity<CarVO> create(@RequestBody @Valid CarDto dto) throws IOException, InterruptedException {
         CarVO car = createCarService.create(dto);
+
+        // Send message to queue
+        rabbitMQService.sendMessage(RabbitMQConstants.CAR_QUEUE, dto);
 
         URI uri = URI.create("api-test.bhut.com.br:3000/api/cars/" + car.get_id());
 
